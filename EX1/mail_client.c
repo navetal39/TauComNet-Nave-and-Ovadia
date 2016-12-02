736c3d4c22fd5578e2fa2f84528c1d184918800b
Nave <navetal@gmail.com>
2016-12-02 13:55:55 +0200

    more client progress
    finished socket creation and connection (except for errors, of course)
    started getting username and password

--- EX1/mail_client.c
+++ EX1/mail_client.c
@@ -7,7 +7,10 @@
 int main(int argc, char* argv[])
 {
 	const int TOTAL_TO = 20, MAXMAILS = 32000, MAX_USERNAME = 50, MAX_PASSWORD = 30, MAX_SUBJECT = 100, MAX_CONTENT = 2000, NUM_OF_CLIENTS = 20;
+	const int WELCOME_LENGTH = 33;
 	const short DEFAULT_PORT = 6423;
+	char welcomeMessage[WELCOME_LENGTH], username[MAX_USERNAME], password[MAX_PASSWORD];
+	int recLen;
 
 	/* Initialize address struct: */
 	struct sockaddr_in serverAddr;
@@ -34,10 +37,14 @@ int main(int argc, char* argv[])
 	}else{
 		serverAddr.sin_port = DEFAULT_PORT;
 	}
-	int sock_des = socket(PF_INET, SOCK_STREAM, 0);
-	success = connect(sock_des, serverAddr, sizeof(serverAddr));
+	int sockDes = socket(PF_INET, SOCK_STREAM, 0);
+	success = connect(sockDes, serverAddr, sizeof(serverAddr));
 	if(success==-1)// error
 	{
 		//TODO errors
 	}
+	recLen = recv(sockDes, welcomeMessage, WELCOME_LENGTH);
+	printf("%s\n", *welcomeMessage);
+	printf("User: ");
+	username = scanf("%s");
 }
