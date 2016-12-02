1c52ef724b51d04a9b69e299ae9a685ac32bb9eb
Nave <navetal@gmail.com>
2016-12-02 13:00:44 +0200

    basic stuff
    a tiny bit of client action

--- EX1/mail_client.c
+++ EX1/mail_client.c
@@ -3,3 +3,19 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <errno.h>
+
+int main(int argc, char* argv)
+{
+	if(argc==2)
+	{
+		/*ip+port were given*/
+	}
+	if(argc==1)
+	{
+		/*ip was given, default port*/
+	}
+	if(argc==0)
+	{
+		/*default ip and port*/
+	}
+}
